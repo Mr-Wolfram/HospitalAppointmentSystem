e20e8a5df7230a498f7cd2d66899db024ceb3036
permui <aoyang.yu.acc@outlook.com>
2022-05-30 11:58:19 +0800

    delete backend files

--- backend/dbinit.js
+++
@@ -1,8 +0,0 @@
-conn = new Mongo();
-
-db = conn.getDB("admin");
-db.auth("root", "example");
-
-db = conn.getDB("test");
-
-db.createUser({ user: "ReadWriter", pwd: "ReadWriter", roles: ["readWrite"] });

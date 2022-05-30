10e2515e1170b40de24895165a1c0d9c9a480539
maxreo <1183578406@qq.com>
2022-05-30 16:41:03 +0800

    thx 增加getavatar，增加个人信息完善

--- frontend/frontend_user/src/mock/userinfo.js
+++ frontend/frontend_user/src/mock/userinfo.js
@@ -12,12 +12,15 @@ export default [
             const generateData = () => {
                 let unit={
                     phonenumber:"13500000000",
-                    IDnum:"35111111111111111",
                     username:"张三",
                     age:"18",
                     email:"123333@qq.com",
-                    avatar:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.meituan.net%2Favatar%2F8e121229f5fbde1040d0969a095afdbf49867.jpg&refer=http%3A%2F%2Fimg.meituan.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656347237&t=a2e060b70f467b8b6171d71baa64485c",
                     user_id:user_id,
+                    gender:"男",
+                    hereditary:"中风",
+                    pastill:"哮喘、心脏病",
+                    height:"178cm",
+                    weight:"65kg"
                 }
                 return_list.push(unit);
             };
@@ -58,6 +61,46 @@ export default [
         response: config => {
             return {
                 code:200,
+            }
+        }
+    },
+    {
+        url: '\/api\/user\/getavatar/\*',
+        type: 'get',
+
+        response: config => {
+            return {
+                code:200,
+                data:[{
+                    url:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.meituan.net%2Favatar%2F8e121229f5fbde1040d0969a095afdbf49867.jpg&refer=http%3A%2F%2Fimg.meituan.net&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1656347237&t=a2e060b70f467b8b6171d71baa64485c"
+                }]
+            }
+        }
+    },
+    {
+        url: '\/api\/user\/set_info/\*',
+        type: 'post',
+
+        response: config => {
+            let params=JSON.parse(config.body).params;
+            const user_id=params.user_id;
+            const gender=params.gender;
+            const age=params.age;
+            const hereditary=params.hereditary;
+            const pastill=params.pastill;
+            const height=params.height;
+            const weight=params.weight;
+            return {
+                code:200,
+                data:[{
+                    user_id:user_id,
+            gender:gender,
+            age:age,
+            hereditary:hereditary,
+            pastill:pastill,
+            height:height,
+            weight:weight
+                }]
             }
         }
     },

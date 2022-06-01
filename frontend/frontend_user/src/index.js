ec2f3d5a5fc8a04494ac1728b3062ad02bd379de
Ji Ma <zjumaji@zju.edu.cn>
2022-06-01 19:45:03 +0800

    del mock,use backend

--- frontend/frontend_user/src/index.js
+++ frontend/frontend_user/src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 
 // mock开关，设置是否引入文件
-const mock = true
-if (mock) {
-  require('./mock/index') 
-}
+// const mock = true
+// if (mock) {
+//   require('./mock/index')
+// }
 
 ReactDOM.render(
       <BrowserRouter>

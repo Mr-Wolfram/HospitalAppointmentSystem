ccc65267cced067f25fcc9cd198ba74f4eaddea7
Ji Ma <zjumaji@zju.edu.cn>
2022-06-09 21:25:46 +0800

    change token for private info

--- frontend/frontend_user/src/commons/request.js
+++ frontend/frontend_user/src/commons/request.js
@@ -7,13 +7,12 @@ let http = {
     get: ""
 }
 
-let tokenDefault="Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyOTQ1OGE5MGFhYjBlOWVkMjhmMzUyOSIsImlhdCI6MTY1MzkxMjg0NTg4NywiZXhwIjoxNjUzOTEyOTMyMjg3fQ.Q30CSEDDrdOazSOuAhVC4zl8d1Sahvnaj25gN7Is9eLkxZg7HCq3-qgK37S5ajJfacrIXu14Zx6jAsG8XKWoLNCeZJcZOSfBkM21UR_VQ9L08ZTr06ZTVCSDVFNDpX_KZQDC4Vgzw3Msv9Sjw4eLXEiQ-tQJeWrNHQD5LZ9VBj-wZe_VJBKAfzzlptimD53gc6Z6jkA0hUS5fxtCMH7Eza5goh8Zm3NF_IZcPhpWjLgz0EURredZ8-rvFlQYJxbJdzTDD0hO4mRFannnzsyGxLaF_Say9lnd08daZKQ5xLd_bazJjujEffYhxDEBCkmOakbG5LIDFG9bsEm5Tcs0eA"
-
+let tokenDefault="";
 
 http.post = function(api, data) {
     axios.defaults.headers.common = {
         // 'Authorization': 'Bearer ' + tokenDefault
-        'Authorization': cookie.load("token")===undefined?tokenDefault:cookie.load("token")
+        // 'Authorization': cookie.load("token")===undefined?tokenDefault:cookie.load("token")
     };
     return new Promise((resolve, reject) => {
         axios.post(api, {
@@ -33,7 +32,7 @@ http.get = function(api, data) {
     return new Promise((resolve, reject) => {
         axios.defaults.headers.common = {
             // 'Authorization': 'Bearer ' + tokenDefault
-            'Authorization': cookie.load("token")===undefined?tokenDefault:cookie.load("token")
+            // 'Authorization': cookie.load("token")===undefined?tokenDefault:cookie.load("token")
         };
         axios.get(api, {
             params: data,
